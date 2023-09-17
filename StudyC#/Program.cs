01bdefd0501bac92823818a84d0910387490c9bf
Alexandr <saniabublik5@gmail.com>
2023-09-17 08:47:59 +0300

    Добавьте файлы проекта.

---
+++ StudyC#/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+namespace Program.cs
+
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Enter text: ");
+            string text = Console.ReadLine();
+            using (FileStream stream = new FileStream("info.txt", FileMode.Open))
+            {
+                byte[] array = System.Text.Encoding.Default.GetBytes(text);
+                stream.Write(array, 0, array.Length);
+            }
+
+
+
+        }
+    }
+        
+}
